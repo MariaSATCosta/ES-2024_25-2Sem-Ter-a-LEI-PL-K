041caba77198a207013e164e01749e7f3140543f
MariaSATCosta <msatc1@iscte-iul.pt>
2025-04-04 01:07:24 +0100

    criacao funcionalidades de conexao ao neo4j e inserção de dados nos nodes

--- src/main/java/com/gestaodeterritorio/App.java
+++ src/main/java/com/gestaodeterritorio/App.java
@@ -5,14 +5,25 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         try {
+            Neo4jConnector connector = new Neo4jConnector();
+
+
             CadastroLoader loader = new CadastroLoader();
             List<PropriedadeRustica> propriedades = loader.carregar("Madeira-Moodle.csv");
 
             for (PropriedadeRustica p : propriedades) {
                 System.out.println(p);
+                connector.addPropriedade(p);
             }
+
+
+
+            connector.close();
         } catch (Exception e) {
             System.err.println("Erro ao carregar o CSV: " + e.getMessage());
         }
+
+
+
     }
 }
--- src/main/java/com/gestaodeterritorio/Neo4jConnector.java
+++ src/main/java/com/gestaodeterritorio/Neo4jConnector.java
@@ -1,4 +1,47 @@
 package com.gestaodeterritorio;
 
-public class Neo4jConnector {
+import io.github.cdimascio.dotenv.Dotenv;
+import org.neo4j.driver.*;
+
+public class Neo4jConnector implements AutoCloseable {
+    private final Driver driver;
+
+    public Neo4jConnector() {
+        Dotenv dotenv = Dotenv.load();
+        String uri = dotenv.get("NEO4J_URI");
+        String user = dotenv.get("NEO4J_USER");
+        String password = dotenv.get("NEO4J_PASSWORD");
+
+        driver = GraphDatabase.driver(uri, AuthTokens.basic(user, password));
+    }
+
+    @Override
+    public void close() {
+        driver.close();
+    }
+
+    public void addPropriedade(PropriedadeRustica prop) {
+        try (Session session = driver.session()) {
+            String query = "CREATE (:Propriedade {objectId: $objectId, parId: $parId, parNum: $parNum, municipio: $municipio, " +
+                    "freguesia: $freguesia, shapeArea: $shapeArea, ilha: $ilha, geometry: $geometry}) RETURN 1";
+
+            session.writeTransaction(tx -> {
+                Result result = tx.run(query, Values.parameters(
+                        "objectId", prop.getObjectId(),
+                        "parId", prop.getParId(),
+                        "parNum", prop.getParNum(),
+                        "municipio", prop.getMunicipio(),
+                        "freguesia", prop.getFreguesia(),
+                        "shapeArea", prop.getShapeArea(),
+                        "ilha", prop.getIlha(),
+                        "geometry", prop.getGeometry()));
+
+                result.consume();
+                return null;
+            });
+        }
+    }
+    
+
+
 }
